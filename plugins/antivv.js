3750cab49dd3bdcd64340c0b875defce79ceb536
Jupiter Bold <sanadababy5@gmail.com>
2024-10-20 06:39:48 +0100

    ᴀʙᴏᴠᴇ ᴀʟʟ

--- plugins/antivv.js
+++ plugins/antivv.js
@@ -1,10 +1,12 @@
 const { smd, bot_ } = require("../lib");
 let bgmm = false;
+
+// Command to enable or disable the AntiViewOnce feature
 smd(
   {
     cmdname: "antiviewonce",
     alias: ["antivv"],
-    desc: "turn On/Off auto viewOnce Downloder",
+    desc: "Turn On/Off auto ViewOnce Downloader",
     fromMe: true,
     type: "user",
     use: "<on/off>",
@@ -12,6 +14,7 @@ smd(
   },
   async (_0x5c3dd1, _0x543e4e) => {
     try {
+      // Retrieve or create the bot settings for the user
       bgmm =
         (await bot_.findOne({
           id: "bot_" + _0x5c3dd1.user,
@@ -19,7 +22,9 @@ smd(
         (await bot_.new({
           id: "bot_" + _0x5c3dd1.user,
         }));
+      
       let _0x446f76 = _0x543e4e.toLowerCase().split(" ")[0].trim();
+      
       if (_0x446f76 === "on" || _0x446f76 === "enable" || _0x446f76 === "act") {
         if (bgmm.antiviewonce === "true") {
           return await _0x5c3dd1.reply("*AntiViewOnce already enabled!*");
@@ -32,7 +37,7 @@ smd(
             antiviewonce: "true",
           }
         );
-        return await _0x5c3dd1.reply("*AntiViewOnce Succesfully enabled*");
+        return await _0x5c3dd1.reply("*AntiViewOnce successfully enabled*");
       } else if (
         _0x446f76 === "off" ||
         _0x446f76 === "disable" ||
@@ -49,20 +54,22 @@ smd(
             antiviewonce: "false",
           }
         );
-        return await _0x5c3dd1.reply("*AntiViewOnce Succesfully deactivated*");
+        return await _0x5c3dd1.reply("*AntiViewOnce successfully deactivated*");
       } else {
         return await _0x5c3dd1.send(
-          "*_Use on/off to enable/disable antiViewOnce!_*"
+          "*_Use on/off to enable/disable AntiViewOnce!_*"
         );
       }
     } catch (_0x4bb48d) {
       await _0x5c3dd1.error(
-        _0x4bb48d + "\n\nCommand: AntiViewOnce ",
+        _0x4bb48d + "\n\nCommand: AntiViewOnce",
         _0x4bb48d
       );
     }
   }
 );
+
+// Handler for viewonce messages
 smd(
   {
     on: "viewonce",
@@ -86,21 +93,20 @@ smd(
         let _0x58b72c = await _0x4a4a25.bot.downloadAndSaveMediaMessage(
           _0x4a4a25.msg
         );
+
+        // Send the downloaded media to the user's DM
         await _0x4a4a25.bot.sendMessage(
-          _0x4a4a25.from,
+          _0x4a4a25.user,  // Sending to user's DM
           {
             [_0x4a4a25.mtype2.split("Message")[0]]: {
               url: _0x58b72c,
             },
             caption: _0x4a4a25.body,
-          },
-          {
-            quoted: _0x52bb9a,
           }
         );
       }
     } catch (_0x6010c1) {
-      console.log("error while getting antiviewOnce media\n, ", _0x6010c1);
+      console.log("Error while getting AntiViewOnce media: ", _0x6010c1);
     }
   }
 );
